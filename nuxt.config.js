391dbb3abf691af0e69bd5d88ced3e9b3aabf95f
Arsanandha Abhisidvorachodi <arsanandha.ap@gmail.com>
2023-01-06 12:43:35 +0700

    Update to 25

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: '24',
+    title: '25',
     htmlAttrs: {
       lang: 'en'
     },
@@ -18,6 +18,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/main.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -32,8 +33,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    '@nuxt/postcss8',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -42,5 +42,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="text-center">
-    <h1 class="text-2xl" @click="confetti">
-      🎉
-    </h1>
-    {{ message }}
+  <div class="text-center hero min-h-screen">
+    <div class="hero-content flex-col lg:flex-row">
+      <img src="../assets/25V2.svg" class="mx-auto items-center light:invert w-72" @click="confetti"/>
+      <div class="mockup-code text-left">
+        <pre data-prefix="1"><code>let mybirthday: BirthdayDto = {};</code></pre>
+        <pre data-prefix="2"><code>mybirthday = {</code></pre>
+        <pre data-prefix="3" class="text-success"><code>  "message": "{{ message['message'] }}",</code></pre>
+        <pre data-prefix="4" class="text-success"><code>  "additional": "{{ message['additional'] }}"</code></pre>
+        <pre data-prefix="5" class="text-success"><code>  "age": {{ message['age'] }}</code></pre>
+        <pre data-prefix="6" class="text-success"><code>  "born_in": "{{ message['born_in'] }}"</code></pre>
+        <pre data-prefix="7"><code>};</code></pre>
+        <pre data-prefix="8"><code>console.log(mybirthday);</code></pre>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
-@tailwind base;
-@tailwind utilities;
-@tailwind components;
+
 </style>
 <script lang="ts">
 import Vue from 'vue'
@@ -24,17 +31,41 @@ export default Vue.extend({
         "message":"Happy Birthday to Me !",
         "additional":`🎉🎉🎉🎉🎉🎉🎉`,
         "age":age,
-        "born_in":dateTime.getFullYear()
+        "born_in": dateTime.getFullYear()
       }
     }
   },
   methods:{
     confetti: function(){
-      (this as any).$confetti.start();
+      (this as any).$confetti.start({
+        particles: [
+          {
+            type: 'heart',
+          },
+          {
+            type: 'circle'
+          }
+        ],
+        defaultDropRate: 30,
+        defaultSize: 20,
+        defaultColors: [
+          '#ffdf00',
+          '#d4af37',
+          '#996515'
+        ],
+      });
       setTimeout(()=>{
         (this as any).$confetti.stop();
       },20000);
     }
   },
+  mounted() {
+    console.log({
+      "message": this.message.message,
+      "additional": this.message.additional,
+      "age": this.message.age,
+      "born_in": this.message.born_in
+    })
+  }
 })
 </script>
